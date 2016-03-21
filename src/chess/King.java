b0cfe55838487a1d337e0087967ad11da36b7e56
kidacki@yahoo.com <kidacki@yahoo.com>
2016-03-21 15:44:14 -0400

    implemented the getValidMoves() for King but I don't know how to do left castling and right castling because we need to check if left rook is unmoved and if king is unmoved for left castling then for right castling we need to check if right rook is unmoved and king is unmoved and ofcourse if the space between them is empty

--- src/chess/King.java
+++ src/chess/King.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import chess.Chess.RankFile;
 
 public class King extends Piece {
-boolean unmoved;
+	boolean unmoved;
+
 	public King(char color, RankFile position, Board board) {
 		super(color, position, board);
 		this.type = 'K';
@@ -16,44 +17,70 @@ boolean unmoved;
 	ArrayList<RankFile> getValidMoves() {
 		int r = this.position.rank;
 		char f = this.position.file;
-		
-
+		RankFile square = new RankFile(r, (char) (f));
+		char t = board.getPiece(square).color;
 		ArrayList<RankFile> ans = new ArrayList<RankFile>();
 
-		//top row
-		if (r != 8){
-			//top-left
-			if (f != 'a'){
-				ans.add(new RankFile(r+1, (char)(f-1)));
-			}
-			//top
-			ans.add(new RankFile(r+1, f));
-			//top-left
-			if (f != 'h'){
-				ans.add(new RankFile(r+1, (char)(f+1)));
-			}
+		// castling-left-rook-incomplete
+		if(unmoved){
+		RankFile leftSquare = new RankFile(r , (char) (f-1));
+		RankFile leftLeftSquare= new RankFile(r, (char) (f-2));
+		RankFile leftRook= new RankFile(r, (char) (f-3)); // have to check if its unmoved, not implemented
+		if(board.getPiece(leftSquare) == null&&board.getPiece(leftLeftSquare)==null){
+
 		}
-		//left
-		if (f != 'a'){
-			ans.add(new RankFile(r, (char)(f-1)));
 		}
-		//right
-		if (f != 'h'){
-			ans.add(new RankFile(r, (char)(f+1)));
+		// castling-right-rook-incomplete
+
+		// up
+		square = new RankFile(r + 1, (char) (f));
+		if (r + 1 > 0 && r + 1 < 9 && f >= 'a' && f <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r + 1, (char) (f)));
 		}
-		//bottom row
-		if (r != 1) {
-			//bottom-left
-			if (f != 'a'){
-				ans.add(new RankFile(r-1, (char)(f-1)));
-			}
-			//bottom
-			ans.add(new RankFile(r-1, f));
-			//bottom-right
-			if (f != 'h'){
-				ans.add(new RankFile(r-1, (char)(f+1)));
-			}
+		// up-left
+		square = new RankFile(r + 1, (char) (f - 1));
+		if (r + 1 > 0 && r + 1 < 9 && f - 1 >= 'a' && f - 1 <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r + 1, (char) (f - 1)));
 		}
+		// up-right
+		square = new RankFile(r + 1, (char) (f + 1));
+		if (r + 1 > 0 && r + 1 < 9 && f + 1 >= 'a' && f + 1 <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r + 1, (char) (f + 1)));
+		}
+		// left
+		square = new RankFile(r, (char) (f - 1));
+		if (r > 0 && r < 9 && f - 1 >= 'a' && f - 1 <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r, (char) (f - 1)));
+		}
+		// right
+		square = new RankFile(r, (char) (f + 1));
+		if (r > 0 && r < 9 && f + 1 >= 'a' && f + 1 <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r, (char) (f + 1)));
+		}
+		// down
+		square = new RankFile(r - 1, (char) (f));
+		if (r - 1 > 0 && r - 1 < 9 && f >= 'a' && f <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r - 1, (char) (f)));
+		}
+		// down-right
+		square = new RankFile(r - 1, (char) (f + 1));
+		if (r - 1 > 0 && r - 1 < 9 && f + 1 >= 'a' && f + 1 <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r - 1, (char) (f + 1)));
+		}
+		// down-left
+		square = new RankFile(r - 1, (char) (f - 1));
+		if (r - 1 > 0 && r - 1 < 9 && f - 1 >= 'a' && f - 1 <= 'h'
+				&& (board.getPiece(square) == null || board.getPiece(square).color != t)) {
+			ans.add(new RankFile(r - 1, (char) (f - 1)));
+		}
+
 		return ans;
 	}
 }
